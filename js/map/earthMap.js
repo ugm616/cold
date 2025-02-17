26969802e8eee58ed54136105e9e9eb7eb501b69
ugm616 <67227131+ugm616@users.noreply.github.com>
2025-02-17 15:43:10 +0000

    Update earthMap.js

--- js/map/earthMap.js
+++ js/map/earthMap.js
@@ -422,3 +422,22 @@ class EarthMap {
     this.zoomLevel = newZoom;
     this.render();
 }
+waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
