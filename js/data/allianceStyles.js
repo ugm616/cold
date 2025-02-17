714928251ffd66a9863dc1d25c550c406f0e8d0c
ugm616 <67227131+ugm616@users.noreply.github.com>
2025-02-17 15:32:39 +0000

    Create allianceStyles.js

---
+++ js/data/allianceStyles.js
@@ -0,0 +1,32 @@
+export const ALLIANCE_STYLES = {
+    "NATO": {
+        color: "#0066CC",
+        highlightColor: "#3399FF",
+        borderColor: "#003366"
+    },
+    "WARSAW_PACT": {
+        color: "#CC0000",
+        highlightColor: "#FF3333",
+        borderColor: "#660000"
+    },
+    "NON_ALIGNED": {
+        color: "#666666",
+        highlightColor: "#999999",
+        borderColor: "#333333"
+    },
+    "WESTERN_ALIGNED": {
+        color: "#0099CC",
+        highlightColor: "#33CCFF",
+        borderColor: "#006699"
+    },
+    "SOVIET_ALIGNED": {
+        color: "#990000",
+        highlightColor: "#CC3333",
+        borderColor: "#660000"
+    },
+    "NEUTRAL": {
+        color: "#339933",
+        highlightColor: "#66CC66",
+        borderColor: "#006600"
+    }
+};
